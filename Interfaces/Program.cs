81bd5d1606878476f5c2fbcbaf1e9f4b8afdda2c
santi <santi@DESKTOP-PN1MNH7>
2023-07-06 17:56:00 -0700

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conejo conejo = new Conejo();
+            Aguila aguila = new Aguila();
+            Pez pez = new Pez();
+
+            conejo.Huir();
+            aguila.Cazar();
+            pez.Cazar();
+            pez.Huir();
+        }
+    }
+
+    interface IPreza
+    {
+        void Huir();
+    }
+    interface IPredador
+    {
+        void Cazar();
+    }
+    class Conejo : IPreza
+    {
+        public void Huir()
+        {
+            Console.WriteLine("La preza huye");
+        }
+    }
+    class Aguila : IPredador
+    {
+        public void Cazar()
+        {
+            Console.WriteLine("El predador caza");
+        }
+    }
+    class Pez : IPreza, IPredador
+    {
+        public void Huir()
+        {
+            Console.WriteLine("El Peza huye por que es preza");
+        }
+
+        public void Cazar()
+        {
+            Console.WriteLine("El Peza caza por que es predador");
+        }
+    }
+}
